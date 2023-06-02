cc44208b76905d0a04095730730e70fa8fd773e0
Vaiva <vaivav11@gmail.com>
2023-06-02 07:18:36 +0300

    Update redaguoti.php

--- admin/redaguoti.php
+++ admin/redaguoti.php
@@ -1,6 +1,6 @@
 <?php
 include 'db_connect.php';
-$qry = $conn->query("SELECT * FROM categories where id = ".$_GET['id'])->fetch_array();
+$qry = $conn->query("SELECT * FROM uzimtumas where id = ".$_GET['id'])->fetch_array();
 foreach($qry as $k => $v){
 	$$k = $v;
 }
